f0d411c9923ace6c6e2281f5562bf25b24f8378b
Ahmed AbdElnasser <133560043+AbdElnasser77@users.noreply.github.com>
2026-06-11 19:03:15 +0300

    Revert "Chore/remove GitHub folder"

--- apps/roseApp/src/app/remote-entry/entry.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-rose-entry',
-  template: `<h1 class="bg-blue-500">RoseApp</h1>`,
-})
-export class RemoteEntry {}
