2ee5c94adedf0befea9ef15cf2c6fd8a88fda2ba
Eman Ahmed <eman455sallam@gmail.com>
2026-06-14 19:01:54 +0300

    creating toast component

---
+++ libs/ui/src/lib/components/toast/toast.component.ts
@@ -0,0 +1,26 @@
+// import { Component, inject } from '@angular/core';
+// import { ToastService } from '@org/shared-util-notification';
+
+
+// @Component({
+//   selector: 'lib-toast',
+//   imports: [ ],
+//   templateUrl: './toast.component.html',
+//   styleUrl: './toast.component.scss',
+
+// })
+// export class ToastComponent {
+//       private toastService = inject(ToastService);
+
+//   showSuccess() {
+//     this.toastService.show('Operation completed successfully', 'success');
+//   }
+
+//   showError() {
+//     this.toastService.show('Something went wrong', 'error');
+//   }
+
+//   showInfo() {
+//     this.toastService.show('Message Content', 'info');
+//   }
+// }
