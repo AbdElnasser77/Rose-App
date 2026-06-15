cbad3f7bac890f4a69bf18bc3b98e6fac6f9b183
Eman Ahmed <eman455sallam@gmail.com>
2026-06-15 23:10:57 +0300

    updating size in button component

--- libs/ui/src/lib/components/button/button.component.ts
+++ libs/ui/src/lib/components/button/button.component.ts
@@ -14,7 +14,7 @@ export class ButtonComponent {
   @Input() type: 'button' | 'submit' = 'button';
   @Input() disabled = false;
   @Input() loading = false;
-  @Input() size: 'full' | 'half' | 'quarter' | 'three-quarters' | 'auto' = 'full';
+  @Input() size: 'full' | 'half' | 'auto' = 'full';
   @Input() variant: 'Primary' | 'Secondary' | 'Outline' | 'Subtle' | 'Ghost' | 'Destructive' = 'Primary';
 
   @Output() clicked = new EventEmitter<MouseEvent>();
