cbe3b216e0ea132d3dc81865b8439c85f9df9bc6
Eman Ahmed <eman455sallam@gmail.com>
2026-06-19 00:13:19 +0300

    fix/app-ts-conflict

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LanguageService } from '@rose/i18n';
 import { ToastContainerComponent } from '@org/ui';
-import { ToastService } from '@org/shared-util-notification';
 @Component({
   imports: [RouterModule,ToastContainerComponent],
   selector: 'app-root',
@@ -12,14 +11,9 @@ import { ToastService } from '@org/shared-util-notification';
 })
 export class App implements OnInit {
   private languageService = inject(LanguageService);
-  constructor(private toast: ToastService) {}
 
+  
   ngOnInit(): void {
     this.languageService.initialize();
-    this.loginSuccess()
   }
-
-  loginSuccess() {
-  this.toast.show('Login successful', 'success');
-}
 }
