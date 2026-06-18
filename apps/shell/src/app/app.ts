b36a241515773dee5463f3aff9b33e4b9bf01843
Eman Ahmed <eman455sallam@gmail.com>
2026-06-18 23:03:20 +0300

    fix: improve toast responsiveness layout

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LanguageService } from '@rose/i18n';
 import { ToastContainerComponent } from '@org/ui';
-
+import { ToastService } from '@org/shared-util-notification';
 @Component({
   imports: [RouterModule,ToastContainerComponent],
   selector: 'app-root',
@@ -12,10 +12,14 @@ import { ToastContainerComponent } from '@org/ui';
 })
 export class App implements OnInit {
   private languageService = inject(LanguageService);
+  constructor(private toast: ToastService) {}
 
   ngOnInit(): void {
     this.languageService.initialize();
+    this.loginSuccess()
   }
 
-
+  loginSuccess() {
+  this.toast.show('Login successful', 'success');
+}
 }
