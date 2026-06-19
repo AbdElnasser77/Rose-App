56217afc3bd50c7010e9f409d1566390c8d629aa
Eman Ahmed <eman455sallam@gmail.com>
2026-06-19 14:44:49 +0300

    add send email verification form

--- apps/roseApp/src/app/features/auth/pages/register/register.component.ts
+++ apps/roseApp/src/app/features/auth/pages/register/register.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { WelcomeMessageComponent, DividerComponent, ReusableInputComponent, ButtonComponent, CalloutTextComponent, SelectInputComponent } from '@org/ui';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [WelcomeMessageComponent, DividerComponent, ReusableInputComponent, ButtonComponent, CalloutTextComponent, SelectInputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+    isLoading:boolean=false;
+    gender=[
+      {label:'MALE',value :'male'},
+      {label :'FEMALE',value:'female'}
+    ]
+
+}
--- apps/roseApp/src/app/features/auth/pages/send-email-verification/send-email-verification.component.ts
+++ apps/roseApp/src/app/features/auth/pages/send-email-verification/send-email-verification.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthFacade } from '@org/auth';
 import {WelcomeMessageComponent,DividerComponent,ReusableInputComponent,ButtonComponent,CalloutTextComponent} from '@org/ui';
 @Component({
   selector: 'app-send-email-verification',
-  imports: [WelcomeMessageComponent,DividerComponent,ReusableInputComponent,ButtonComponent,CalloutTextComponent],
+  imports: [ReactiveFormsModule,WelcomeMessageComponent,DividerComponent,ReusableInputComponent,ButtonComponent,CalloutTextComponent],
   templateUrl: './send-email-verification.component.html',
   styleUrl: './send-email-verification.component.scss',
 })
 export class SendEmailVerificationComponent {
-  isLoading:boolean=false;
-  submitSendEmailVerfication(){
+     private _fb=inject(FormBuilder);
+    private readonly  _router = inject(Router);
+    private _authFacade=inject(AuthFacade);
 
+
+  isLoading:boolean=false;
+  sendEmailVerificationForm :FormGroup=this._fb.group({
+    email:['',[Validators.required,Validators.email]]
+  });
+  onSendEmailVerificationSubmit(){
+    if(this.sendEmailVerificationForm.invalid){
+      this.sendEmailVerificationForm.markAllAsTouched();
+      return;
+    }
+    this.isLoading=true;
+    const emailValue=this.sendEmailVerificationForm.value.email;
+    this._authFacade.sendEmailVerification(emailValue).subscribe({
+      next :()=>{
+        this.isLoading=false;
+        this._router.navigate(['auth/confirm-email-verification']);
+      }
+    })
   }
 }
