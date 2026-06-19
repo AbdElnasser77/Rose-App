3882bc550a8ac9d3c53f57644da6d33b17924bf0
AbdElnasser77 <a7med3bnaser77@gmail.com>
2026-06-19 20:47:54 +0300

    feat: implemented login page and wired the auth routes and added guest guard.

---
+++ apps/roseApp/src/app/features/auth/pages/login.page.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonComponent, ReusableInputComponent, DividerComponent } from '@org/ui';
+import { AuthFacade, AuthStore } from '@org/auth';
+import { Check, LucideAngularModule } from "lucide-angular";
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, RouterModule, ReusableInputComponent, ButtonComponent, DividerComponent, LucideAngularModule],
+  templateUrl: './login.page.html',
+  styleUrl: './login.page.scss',
+})
+export class LoginPage {
+  private readonly authFacade = inject(AuthFacade);
+  private readonly router = inject(Router);
+  readonly authStore = inject(AuthStore);
+  readonly Check = Check;
+
+  loginForm = inject(FormBuilder).nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    rememberMe: [false],
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+    const { username, password, rememberMe } = this.loginForm.getRawValue();
+    this.authFacade.login({ username, password }, rememberMe).subscribe({
+      next: () => {
+        this.router.navigate(['/home']);
+      },
+    });
+  }
+}
--- apps/roseApp/src/app/remote-entry/entry.routes.ts
+++ apps/roseApp/src/app/remote-entry/entry.routes.ts
@@ -1,26 +1,19 @@
 import { Route } from '@angular/router';
-import { RemoteEntry } from './entry';
-import { MainLayoutComponent } from '../layouts/main-layout/main-layout.component';
 
 export const remoteRoutes: Route[] = [
-  
-   {
-    path: '',
-    component: MainLayoutComponent,
-  },
-  
-  
-  
-  
   {
     path: '',
     redirectTo: 'auth',
     pathMatch: 'full',
-    // component: RemoteEntry,
   },
   {
     path: 'auth',
     loadChildren: () =>
       import('../features/auth/auth.routes').then((m) => m.AuthRoutes),
+  },
+  {
+    path: 'home', // temporary routing. 
+    loadComponent: () =>
+      import('../layouts/main-layout/main-layout.component').then((m) => m.MainLayoutComponent),
   },
 ];
