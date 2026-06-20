aa52fccdc66bbe35ed2c5e890f89cd67342683fc
AbdElnasser77 <a7med3bnaser77@gmail.com>
2026-06-20 15:55:24 +0300

    edited footer design for dark mode

--- apps/roseApp/src/app/shared/components/footer/footer.component.ts
+++ apps/roseApp/src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { LanguageSwitcherComponent } from '@rose/i18n';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [LanguageSwitcherComponent, TranslatePipe],
+  imports: [TranslatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
