7130f91bc8cf98d5fabfa72d2b34a127bc8229c8
Eman Ahmed <eman455sallam@gmail.com>
2026-06-28 20:00:16 +0300

    feat(rose-app): add features bar component with figma styles and lucide icons

--- apps/roseApp/src/app/features/home/components/features-bar/features-bar.component.ts
+++ apps/roseApp/src/app/features/home/components/features-bar/features-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@org/ui';
-import { LucideAngularModule, Truck } from 'lucide-angular';
+import { Headset, LucideAngularModule, RefreshCw, ShieldCheck, Truck } from 'lucide-angular';
 
 @Component({
   selector: 'app-features-bar',
@@ -9,5 +9,11 @@ import { LucideAngularModule, Truck } from 'lucide-angular';
   styleUrl: './features-bar.component.scss',
 })
 export class FeaturesBarComponent {
-  readonly Truck=Truck;
+  
+  features = [
+    { icon: Truck, title: 'Free Delivery', desc: 'For orders above 120 EGP' },
+    { icon: RefreshCw, title: 'Get Refund', desc: 'Refunds within 30 days' },
+    { icon: ShieldCheck, title: 'Safe Payment', desc: '100% Secure Payment' },
+    { icon: Headset, title: '24/7 Support', desc: 'Contact us at any time' }
+  ];
 }
