13039ac088ec87a88b6e4c59d6ec8b8f10f58555
AbdElnasser77 <a7med3bnaser77@gmail.com>
2026-07-02 15:08:15 +0300

    feat: add products list page with grid, skeleton loading, and pagination

--- apps/roseApp/src/app/remote-entry/entry.routes.ts
+++ apps/roseApp/src/app/remote-entry/entry.routes.ts
@@ -4,7 +4,7 @@ import { MainLayoutComponent } from '../layouts/main-layout/main-layout.componen
 export const remoteRoutes: Route[] = [
   {
     path: '',
-    redirectTo: 'auth',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
@@ -13,15 +13,22 @@ export const remoteRoutes: Route[] = [
       import('../features/auth/auth.routes').then((m) => m.AuthRoutes),
   },
   {
-    path:'',
-    component:MainLayoutComponent,
-    children:[
+    path: '',
+    component: MainLayoutComponent,
+    children: [
       {
-        path:'home',
+        path: 'home',
         loadChildren: () =>
-        import('../features/home/home.routes').then((m) => m.HomeRoutes),
+          import('../features/home/home.routes').then((m) => m.HomeRoutes),
+      },
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('../features/products/products.routes').then(
+            (m) => m.ProductsRoutes,
+          ),
       },
       // add products,cart,checkout all here.
-    ]
+    ],
   },
 ];
