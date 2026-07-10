e2e47cc82f74594add708acc92e8dfcead0d6446
Eman Ahmed <eman455sallam@gmail.com>
2026-07-10 21:21:08 +0300

    install swiper and add in best seller section

--- apps/roseApp/src/app/features/home/components/best-selling/best-selling.component.ts
+++ apps/roseApp/src/app/features/home/components/best-selling/best-selling.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal,CUSTOM_ELEMENTS_SCHEMA, AfterViewInit, ViewChild, ElementRef, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'primeng/carousel';
 import { RatingModule } from 'primeng/rating';
@@ -11,6 +11,9 @@ import { ProductCardComponent } from '../../../../shared/components/product-card
 import { Product } from '../../../../shared/models/product.model';
 import {IntersectionObserverDirective } from '@org/util-directives';
 import { ProductDataService } from '../../../products/services/product-details/product-data-api.service';
+import { SwiperDirective } from '@org/util-directives';
+import { SwiperOptions } from 'swiper/types';
+
 
 @Component({
   selector: 'app-best-selling',
@@ -20,55 +23,74 @@ import { ProductDataService } from '../../../products/services/product-details/p
     RatingModule,
     RouterModule,
     TranslatePipe,
-    FormsModule,LucideAngularModule,ButtonComponent,ProductCardComponent ,IntersectionObserverDirective,CommonModule],
+    FormsModule,LucideAngularModule,ButtonComponent,ProductCardComponent ,
+    IntersectionObserverDirective,CommonModule,SwiperDirective],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './best-selling.component.html',
   styleUrl: './best-selling.component.scss',
 })
 export class BestSellingComponent implements OnInit {
+
   public _translateService = inject(TranslateService);
   private productService = inject(ProductDataService);
- private _router = inject(Router);
+  private _router = inject(Router);
 
   readonly ChevronLeft = ChevronLeft;
   readonly ChevronRight = ChevronRight;
   readonly ArrowRight = ArrowRight;
   readonly ArrowLeft = ArrowLeft ;
 
+
   isBestSellingVisible = signal(false);
   products :Product[]=[];
-   isRtl = computed(() => (this._translateService.currentLang()) === 'ar');
+  isRtl = computed(() => (this._translateService.currentLang()) === 'ar');
 
-  responsiveOptions: any[] = [
-  {
-    breakpoint: '1400px',
-    numVisible: 3,
-    numScroll: 1
-  },
-  {
-    breakpoint: '1024px',
-    numVisible: 2,
-    numScroll: 1
-  },
-  {
-    breakpoint: '768px',
-    numVisible: 1,
-    numScroll: 1
-  }
-];
+
   ngOnInit(): void {
     this.getProducts();
   }
 
+ 
   getProducts(): void {
     this.productService.getProduct().subscribe({
       next: (res) => {
         this.products = res.payload.data;
+    
       }
     });
   }
+ 
+
+  get swiperConfig(): SwiperOptions {
+    const rtl=this.isRtl();
+    return{
+      spaceBetween: 24,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: rtl ? '.swiper-button-prev-custom' : '.swiper-button-next-custom',
+      prevEl: rtl ? '.swiper-button-next-custom' : '.swiper-button-prev-custom',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1, 
+      } ,
+      768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+      },
+      1280: {
+        slidesPerView: 3, 
+        spaceBetween: 24,     
+      },
+    },
+
+    }
+    
+  }
+  
 
   handleCardClicked(productId: string){
-    this._router.navigate(['/product-details', productId]);
+    this._router.navigate(['/products', productId]);
 
   }
 
