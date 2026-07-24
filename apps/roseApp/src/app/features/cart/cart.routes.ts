db030905a3e183c4b2e885d168f9e1b4b62d2cd8
Zinab Hashem <zadel@informatique@gmail.com>
2026-07-24 22:18:38 +0300

    authorize cart page

--- apps/roseApp/src/app/features/cart/cart.routes.ts
+++ apps/roseApp/src/app/features/cart/cart.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@org/auth';
 
 export const CartRoutes: Routes = [
   {
     path: '',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./pages/cart/cart.component').then((m) => m.CartComponent),
     title: 'cart',
